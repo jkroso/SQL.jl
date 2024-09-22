a7888fc0308577a25854a412beb91b0700c28e3f
Jake Rosoman <jkroso@gmail.com>
2024-09-22 15:41:07 +1000

    handle null values in update()

--- db.jl
+++ db.jl
@@ -6,11 +6,13 @@
 sqlvalue(m::Date) = format(m, dateformat"yyyy-mm-dd")
 sqlvalue(m::DateTime) = format(m, dateformat"yyyy-mm-ddTHH:MM:SS.sssZ")
 sqlvalue(::Missing) = missing
+sqlvalue(::Nothing) = nothing
 sqlvalue(n::Unsigned) = convert(Int, n)
 sqlvalue(n::Integer) = n
 sqlvalue(n::AbstractFloat) = n
 sqlvalue(s::AbstractString) = s
 
+"Add a value to the `db`"
 save(db::DB, obj) = begin
   table = Symbol(table_name(typeof(obj)))
   names = columns(db, table)
@@ -19,29 +21,38 @@ save(db::DB, obj) = begin
   nothing
 end
 
+"Determine which table a Datatype belongs in"
 table_name(::Type{T}) where T = String(T.name.name)
+
+"Convert a struct to a tuple of values that SQLite can store"
 sqlrow(obj) = map(p->sqlvalue(getproperty(obj, p)), propertynames(obj))
 
+"Update the `db` row associated with `a` have the value of `b` instead"
 update(db::DB, a::T, b::T) where T = begin
-  wheres = []
-  updates = []
+  statements = []=>[]
+  values = []=>[]
   for (c,val) in zip(fieldnames(T), sqlrow(b))
-    push!(getfield(a, c) == getfield(b, c) ? wheres : updates, c=>val)
+    i = (getfield(a, c) == getfield(b, c)) + 1
+    if isnothing(val)
+      i > 1 && push!(statements[i], "'$c' IS Null")
+    else
+      push!(statements[i], "'$c' = ?")
+      push!(values[i], val)
+    end
   end
-  @assert !isempty(updates)
-  sql = "UPDATE \"$(table_name(T))\" SET $(eqwhat(updates)) WHERE $(eqwhat(wheres, " AND "))"
-  r = DBInterface.execute(db, sql, (map(last, updates)..., map(last, wheres)...))
-  !isempty(r)
+  @assert !isempty(statements[2])
+  sql = "UPDATE \"$(table_name(T))\" SET $(join(statements[1], ',')) WHERE $(join(statements[2], " AND "))"
+  !isempty(DBInterface.execute(db, sql, vcat(values[1], values[2])))
 end
 
-eqwhat(pairs, sep=',') = join(("$(p[1])=?" for p in pairs), sep)
-
+"Serialise the `sql` query and create a Vector of the values that should be assigned to its parameters"
 prepare(sql::SQLQuery) = begin
   @dynamic! let variables = []
     sprint(write, sql), variables[]
   end
 end
 
+"Run a query against the `db`. Any keyword arguments will be added to the query as WHERE clauses"
 query(db::DB, sql::SQLQuery; kv...) = begin
   sql = reduce(pairs(kv), init=sql) do out, (k,v)
     out |> SQLFunction{:(=)}([SQLReference(out.table, string(k)), v])
